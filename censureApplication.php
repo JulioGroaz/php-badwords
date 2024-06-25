1fbc219e0da070457916ed9f0b28e205982e631f
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-25 16:19:24 +0200

    file creato, avevo sbagliato cartella di salvataggio

---
+++ censureApplication.php
@@ -0,0 +1,25 @@
+<?php
+$testo = "Mi spingi oltre i miei limiti
+e sento di vivere appieno la mia stessa vita,
+in te ho incontrato me stesso
+e ho guardato oltre,
+oltre ogni inimmaginabile limite.
+Ho guardato nel profondo dei tuoi occhi
+cercando di comprenderti
+ma, ho visto tutto quello che di me
+mai avrei voluto vedere.
+Ho visto la mia fragilità e la mia insicurezza
+i miei sensi di colpa e i miei complessi
+le mie paure e la mia insofferenza
+ho visto le mie tenebre e i miei demoni
+allora, ho guardato ancora oltre
+e nel profondo del mio cuore, un mare in tempesta,
+un oceano immenso dove tuffarsi e perdersi
+e lì nel profondo della mia anima ho compreso!
+Ho provato piacere e orgoglio
+nel capire quello che oggi provo
+nel sapere chi oggi sono veramente
+adesso so che amo le cose belle
+so che amo tutto quello che la vita mi offre
+e una di quelle sei tu."
+?>
